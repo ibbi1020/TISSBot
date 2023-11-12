4547560f119a3e34b65f7f66af82f18b8a073a62
Ibraheem Farooq <ibraheemfrq@gmail.com>
2023-11-12 19:09:34 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import bot
+import responses
+
+if __name__ == '__main__':
+    bot.run_discord_bot()
+
---
+++ responses.py
@@ -0,0 +1,28 @@
+import random
+
+def getReponses(message: str) -> str:
+    pmessage = message.lower()
+    text1 = 'this is so sad'
+    text2 = 'mar jana chaiyan'
+    text3 = 'mar jaatey hain'
+
+    if text1 in pmessage:
+        return ':ss::aa::dd:'
+
+    if text2 in pmessage or text3 in pmessage:
+        if (random.randint(1,2) == 1):
+            return 'fr fr'
+        else:
+            return 'chalo 6th floor'
+
+    if pmessage == 'hello':
+        return 'Kia hai bey?'
+
+    if pmessage == 'roll':
+        return random.randint(1,6)
+
+    if pmessage == '!help':
+        if (random.randint(1,2) == 1):
+            return '`khud samajh bhai mereko nahi maaloom`'
+        else:
+            return '`This is a help message`'

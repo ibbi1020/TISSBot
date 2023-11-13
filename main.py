132077fa4fba80f33c642ad27427b5958951c190
Ibraheem Farooq <ibraheemfrq@gmail.com>
2023-11-13 17:56:16 +0500

    nothing special

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import bot
 import responses
 
+#TO ADD:
+# * kms 
+# * Randomly delete Rehan Message
+# * Randomly say 'jhoot' to Rehan's message
+# * Add commands to display timetable (monday - thurs)
+# * Add commands to display teacher details
+# * Find a way to add assignment upload and deadline notifications and what assignments are active (with deadline).
+
 if __name__ == '__main__':
     bot.run_discord_bot()
 
